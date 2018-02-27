d1cc01673a1a75d03bdfdceb17f7eecc96ef3d5e
Max Robb <maxbenrobb@maxs-mbp.attlocal.net>
2018-02-26 19:21:19 -0500

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,34 +2,69 @@ var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" ,
 "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , 
 "w" , "x" , "y" , "z"];
 
+var guessesSoFar = [];
+
 var wins = 0;
 
 var losses = 0;
 
 var guessLeft = 9;
 
+var computerGuess = getNewComputerGuess();
 
 //how do I make the guess toLowerCase()
+
 document.onkeyup = function(event) {
+
     var guess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(event.keyCode);
+    if (event.keyCode < 65 || event.keyCode > 90) {
+        alert("use a lower case letter");
+        return;
+    }
 
-   if (guess === "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , 
-    "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , 
-    "w" , "x" , "y" , "z") {
-        if (guess === computerGuess) {
-            wins++;
-        } else if (guess === )
+    if (guessesSoFar.indexOf(guess) > -1) {
+        return;
+    }
 
+    guessesSoFar.push(guess);
+
+    console.log(computerGuess);
+
+    guessLeft--;
+    document.getElementById("guess-left").innerHTML = guessLeft;
+
+    var guessText = "";
+    var guessCounter = document.getElementById("guess-counter").innerHTML;
+    
+    if(guessCounter){
+        guessText = guessCounter + ", " + guess;
+    } else {
+        guessText = guess;
+    }
+
+    document.getElementById("guess-counter").innerHTML = guessText;
+
+    if(guessLeft === 0) {
+        alert("You lost. Game is reset. Try again!!!");
+        guessLeft = 9;
+        guessesSoFar = [];
+        computerGuess = getNewComputerGuess();
+    } 
+    else if(guess === computerGuess){
+        guessLeft = 9;
+        guessesSoFar = [];
+        computerGuess = getNewComputerGuess();
+        document.getElementById("win-counter").innerHTML = ++wins;
+        document.getElementById("guess-counter").innerHTML = "";
+        document.getElementById("guess-left").innerHTML = guessLeft;
+    } else {
+        losses++;
+        document.getElementById("loss-counter").innerHTML = losses;
     }
 
 }
 
-
-
-
-var html =
-    "<p>wins: " + wins + "</p>" +
-    "<p>losses: " + losses + "</p>" +
-    "<p>Guesses left: " + guessLeft + "</p>" + 
-    "<p>Your guess so far: " + "</p>";
+function getNewComputerGuess() {
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
